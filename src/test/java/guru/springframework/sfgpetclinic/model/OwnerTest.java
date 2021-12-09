1a11497b4dfdde654f4c03cd9a68a34867c503e7
Alex <azakotyansky@gmail.com>
2021-12-09 09:30:34 -0500

    Update with unit tests.

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,25 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerTest {
+
+    @Test
+    void dependentAssertions() {
+        Owner owner = new Owner(1l, "Joe", "Tester");
+        owner.setCity("Key West");
+        owner.setTelephone("2124569067");
+
+        assertAll("Properties Test",
+                () -> assertAll("Person Properties"),
+                        () -> assertEquals("Joe", owner.getFirstName()),
+                        () -> assertEquals("Tester", owner.getLastName())),
+                () -> assertAll("Owner Properties",
+                        () -> assertEquals("Key West", owner.getCity()),
+                        () -> assertEquals("2124569067", owner.getTelephone())
+                );
+    }
+
+}
