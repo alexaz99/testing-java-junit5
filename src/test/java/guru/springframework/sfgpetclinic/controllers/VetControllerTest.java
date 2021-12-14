cbf280c0415020988993d7767736cc43ec2dc8fe
Alex <azakotyansky@gmail.com>
2021-12-14 11:26:23 -0500

    Update with unit tests.

--- src/test/java/guru/springframework/sfgpetclinic/controllers/VetControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/VetControllerTest.java
@@ -1,7 +1,12 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.fauxspring.Model;
+import guru.springframework.sfgpetclinic.fauxspring.ModelMapImpl;
 import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.SpecialtyService;
 import guru.springframework.sfgpetclinic.services.VetService;
+import guru.springframework.sfgpetclinic.services.map.SpecialityMapService;
+import guru.springframework.sfgpetclinic.services.map.VetMapService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,15 +19,25 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 class VetControllerTest {
 
     VetService vetService;
+    SpecialtyService specialtyService;
+
+    VetController vetController;
 
     @BeforeEach
     void setUp() {
-        vetService = new VetMapService();
-        //vetService = new VetMapService();
+        specialtyService = new SpecialityMapService();
+        vetService = new VetMapService(specialtyService);
+        vetController = new VetController(vetService);
     }
 
     @Test
     void listVets() {
+
+        // this is a manual mock object
+        Model model = new ModelMapImpl();
+
+        String view = vetController.listVets(model);
+
         Set<Vet> vets =  vetService.findAll();
 
         assertNotNull(vets);
---
+++ src/test/java/guru/springframework/sfgpetclinic/fauxspring/ModelTest.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.fauxspring;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ModelTest {
+
+}
