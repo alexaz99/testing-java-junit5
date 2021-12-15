cf28fd6f492e951dcab708631008dd46146f7724
Alex <azakotyansky@gmail.com>
2021-12-15 11:36:44 -0500

    Update with unit tests.

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -29,6 +31,15 @@ class PersonTest implements ModelTest {
         assertAll("Test Props Set",
                 () -> assertEquals("Joe", person.getFirstName(),  "First Name Failed"),
                 () -> assertEquals("Tester", person.getLastName(),  "Last Name Failed"));
+    }
+
+    // more meaningful name
+    @RepeatedTest(value = 10, name = "{displayName} : {currentRepetition} - {totalRepetitions}")
+    //@RepeatedTest(10)
+    @DisplayName("My Repeated Test")
+    @Test
+    void myRepeatedTest() {
+        // todo - impl
     }
 
 }
